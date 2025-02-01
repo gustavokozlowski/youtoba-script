a824109316080a6032dea27ac1a2dca2bf86b729
gustavokozlowski <guskozlowskidev@gmail.com>
2025-02-01 13:08:36 -0300

    refactor(index): server

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config()
 const express = require("express")
 const cors = require('cors')
-const { authenticate } = require('../middlewares/auth,middlewares.js')
+const { authenticate } = require('../middlewares/auth.middlewares.js')
 const { PORT } = process.env
 const playlistRoutes = require('../routes/playlist.routes.js')
 const authRoutes = require('../routes/auth.routes.js')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(cookieParser())
 
-app.use('/playlist', authenticate, playlistRoutes)
+app.use('/playlist', playlistRoutes)
 app.use('/auth', authRoutes)
+
+console.log(PORT)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando liso na porta ${PORT}`)
