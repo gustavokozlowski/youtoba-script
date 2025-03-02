73b18db38e724f252164ea059b1848d756516e60
gustavokozlowski <guskozlowskidev@gmail.com>
2025-03-02 03:21:32 -0300

    feat: add getplaylistitems

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,166 +1,184 @@
-require('dotenv').config()
-const axios = require('axios');
-const { API_KEY } = process.env
-const { getToken } = require('../utils/repository/user.repository');
-const e = require('express');
+require("dotenv").config();
+const axios = require("axios");
+const { API_KEY } = process.env;
+const { getToken } = require("../utils/repository/user.repository");
 
 const getAllPlaylists = async (req, res) => {
-    // const decode = jwt.verify(token, JWT_SECRET);
-    const bearerToken = getToken("bearerToken")
+	// const decode = jwt.verify(token, JWT_SECRET);
+	const bearerToken = getToken("bearerToken");
 
-    if (!bearerToken) {
-        return res.send('Token inválido! Por favor, faça o login novamente!')
-    }
+	if (!bearerToken) {
+		return res.send("Token inválido! Por favor, faça o login novamente!");
+	}
 
-    try {
-        // const decoded = jwt.verify(bearerToken, JWT_SECRET);
-        console.warn('=============================== #GET_ALL_PLAYLISTS ==================================')
-        // console.log("decode: ", decoded)
-        console.log("BEAR TOKEN: ", bearerToken.token)
-        console.log("MSG TOKEN: ", bearerToken.message)
-        const result = await axios({
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${bearerToken.token}`,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            "Accept": "application/json",
-            url: `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&mine=true&key=${API_KEY}`
+	try {
+		// const decoded = jwt.verify(bearerToken, JWT_SECRET);
+		console.warn(
+			"=============================== #GET_ALL_PLAYLISTS ==================================",
+		);
+		// console.log("decode: ", decoded)
+		console.log("BEAR TOKEN: ", bearerToken.token);
+		console.log("MSG TOKEN: ", bearerToken.message);
+		const result = await axios({
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${bearerToken.token}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			Accept: "application/json",
+			url: `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails&mine=true&key=${API_KEY}`,
+		});
 
-        })
+		// console.info(result.headers)
 
-        // console.info(result.headers)
+		return res.status(200).json({
+			mensagem: "DEU BOM CARAAALHO!",
+			resultado: result.data,
+		});
+	} catch (e) {
+		console.error("Error daqueles", e.response);
 
-        return res.status(200).json({
-            mensagem: "DEU BOM CARAAALHO!",
-            resultado: result.data
-        })
-
-    } catch (e) {
-
-        console.error("Error daqueles", e.response)
-
-        return res.status(400).json({ messagem: "deu merda no get heein" })
-    }
-}
+		return res.status(400).json({ messagem: "deu merda no get heein" });
+	}
+};
 
 const getPlaylistInfoById = async (playlistId) => {
-    const bearerToken = getToken("bearerToken")
+	const bearerToken = getToken("bearerToken");
 
-    if (!bearerToken) {
-        return res.send('Token inválido! Por favor, faça o login novamente!')
-    }
+	if (!bearerToken) {
+		return res.send("Token inválido! Por favor, faça o login novamente!");
+	}
 
-    try {
-        const playlistInfo = await axios({
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${bearerToken.token}`,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            "Accept": "application/json",
-            url: `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&playlistId=${playlistId}&key=${API_KEY}`
+	try {
+		const playlistInfo = await axios({
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${bearerToken.token}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			Accept: "application/json",
+			url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`,
+		});
 
-        })
+		const { totalResults } = playlistInfo.data.pageInfo;
 
-        const { contentDetails, localized } = playlistInfo.data.items[0]
-        const playlistLength = contentDetails.itemCount
-        const { title, description } = localized
+		return {
+			mensagem:
+				"========================OLHA ESSA PLAYLIST==============================\n",
+			totalPages: totalResults,
+		};
+	} catch (e) {
+		console.error("Error daqueles:", e);
 
-        return {
-            mensagem: "========================OLHA ESSA PLAYLIST==============================\n",
-            quantity: playlistLength,
-            name: title,
-            description: description
-        }
+		return res.status(400).json({ messagem: "deu merda no get heein" });
+	}
+};
 
-    } catch (e) {
+const getPlaylistItems = async (req, res) => {
+	let initialPlaylist = [];
+	const bearerToken = getToken("bearerToken");
+	const { playlistId } = req.params;
 
-        console.error("Error daqueles", e.response)
+	if (!bearerToken) {
+		return res.send("Token inválido! Por favor, faça o login novamente!");
+	}
 
-        return res.status(400).json({ messagem: "deu merda no get heein" })
-    }
-}
+	try {
+		// const decoded = jwt.verify(token, JWT_SECRET);
 
-const removeItemsDulicated = async (req, res) => {
-    const initialPlaylist = []
-    const bearerToken = getToken("bearerToken")
-    const { playlistId } = req.body
-    // biome-ignore lint/style/useConst: <explanation>
-    let pageLimit = 5
-    // biome-ignore lint/style/useConst: <explanation>
-    let thisPageLimit = 0
+		const result = await axios({
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${bearerToken.token}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			Accept: "application/json",
+			url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`,
+		});
 
-    if (!bearerToken) {
-        return res.send('Token inválido! Por favor, faça o login novamente!')
-    }
+		const { data } = result;
 
-    try {
-        // const decoded = jwt.verify(token, JWT_SECRET);
-        const playlistInfo = await getPlaylistInfoById(playlistId)
-        const { quantity } = playlistInfo
+		const firstList = data.items.map((item) => item);
 
-        const result = await axios({
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${bearerToken.token}`,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            "Accept": "application/json",
-            url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`,
-        }).then((playlistData) => {
-            // biome-ignore lint/style/useConst: <explanation>
-            let nextPage = ""
+		initialPlaylist = [...firstList];
 
-            for (let index = 0; index < quantity; index++) {
-                if (initialPlaylist.length < playlistData.data.items.length) {
-                    playlistData.data.items.map((item) => {
-                        return initialPlaylist.push(item)
-                    })
-                } if (initialPlaylist.length > playlistData.data.items.length) {
-                    axios.request({
-                        method: 'GET',
-                        headers: {
-                            Authorization: `Bearer ${bearerToken.token}`,
-                            Accept: 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        "Accept": "application/json",
-                        url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&part=contentDetails&pageToken=${nextPage}&playlistId=${playlistId}&key=${API_KEY}`
-                    }).then((response) => {
-                        response.data
-                    }).catch(e)
-                }
+		console.info("Mais informações dessa Playlist", result.data);
 
+		let nextPageToken = result.data.nextPageToken;
 
-            }
-        })
+		do {
+			const playlist = await axios({
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${bearerToken.token}`,
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				Accept: "application/json",
+				url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&pageToken=${nextPageToken}&playlistId=${playlistId}&key=${API_KEY}`,
+			});
 
-        result.data
+			const playlistData = playlist.data;
 
-        const playlist = [...result.data]
+			nextPageToken = playlistData.nextPageToken;
 
+			playlistData.items.map((item) => initialPlaylist.push(item));
 
+			if (!playlistData.nextPageToken) nextPageToken = undefined;
+		} while (nextPageToken !== undefined);
 
-        // console.info(result.headers)
-        return res.status(200).json({
-            mensagem: "DEU BOM CARAAALHO!",
-            resultado: result.data
-        })
+		return res.status(200).json({
+			mensagem: "DEU BOM CARAAALHO!",
+			resultado: initialPlaylist,
+		});
+	} catch (e) {
+		console.error("Error daqueles", e);
 
-    } catch (e) {
+		return res.status(400).json({ messagem: "deu merda no get heein" });
+	}
+};
 
-        console.error("Error daqueles", e.response)
-
-        return res.status(400).json({ messagem: "deu merda no get heein" })
-    }
-
-}
+const removeItemsDulicated = () => {
+	// usaremos esses dados dentro dos items para mapealos pelo id e remover suas réplicas
+	//  "contentDetails": {
+	//     "videoId": "huHT9E6d73s",
+	//     "videoPublishedAt": "2023-02-04T16:00:22Z"
+	//   }
+};
 
 module.exports = {
-    getAllPlaylists,
-    removeItemsDulicated
+	getAllPlaylists,
+	// removeItemsDulicated,
+	getPlaylistItems,
 };
+
+// EXEMPLO QUE ESTAVAMOS UTILIZANDO ANTES DE REFINAR A FUNCTION removeItemsDulicated
+// .then((playlistData) => {
+//     // biome-ignore lint/style/useConst: <explanation>
+//     let nextPage = ""
+
+//     for (let index = 0; index < quantity; index++) {
+//         if (initialPlaylist.length < playlistData.data.items.length) {
+//             playlistData.data.items.map((item) => {
+//                 return initialPlaylist.push(item)
+//             })
+//         } if (initialPlaylist.length > playlistData.data.items.length) {
+//             axios.request({
+//                 method: 'GET',
+//                 headers: {
+//                     Authorization: `Bearer ${bearerToken.token}`,
+//                     Accept: 'application/json',
+//                     'Content-Type': 'application/json'
+//                 },
+//                 "Accept": "application/json",
+//                 url: `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=id&part=contentDetails&pageToken=${nextPage}&playlistId=${playlistId}&key=${API_KEY}`
+//             }).then((response) => {
+//                 response.data
+//             }).catch(e)
+//         }
+
+//     }
+// })
