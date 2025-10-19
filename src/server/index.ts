15e9cd7fc02df1fc9543bf932acbcf80132b26a8
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-19 14:06:53 -0300

    refactor: server

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,10 +5,8 @@ import express from 'express';
 
 // import { authenticate } from '../middlewares/auth.middlewares'
 const { PORT } = process.env;
-
 import cookieParser from 'cookie-parser';
-import { router as authRoutes } from '../routes/auth.routes';
-import { router as playlistRoutes } from '../routes/playlist.routes';
+import { routes } from '../routes';
 
 const app = express();
 app.use(express.json());
@@ -16,8 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
-app.use('/playlist', playlistRoutes);
-app.use('/auth', authRoutes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
    console.log(`Servidor rodando liso na porta ${PORT}`);
