e90ec1d46b433c478cb08a90bd39d009567b7520
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 02:02:37 -0300

    refactor: method getCredentials

--- src/services/youtube-playlist.services.ts
+++ src/services/youtube-playlist.services.ts
@@ -1,42 +1,41 @@
 require('dotenv').config();
+
 import axios from 'axios';
-import { Response } from 'express';
+import type { Response } from 'express';
 import { getToken } from '../utils/repository/user.repository';
 
 const { API_KEY } = process.env;
 
-export class YoutubeService {
-    bearerToken: string | undefined;
-    constructor() {
+export class YoutubeService { 
+token: string | undefined;
 
-        this.bearerToken = undefined;
+constructor(){
+this._getCredentials()
 }
 
-    private async _getCredentials(){
-    const { token } = await getToken("bearerToken");
+    private async _getCredentials() {
+        const { token } = await getToken('bearerToken');
 
-    if (!token || typeof token === undefined) {
-      console.info("DEU MERDA AQUI NA PORRA DO TOKEN DO YOUTOBA HEIN PQP!");
-      throw new Error('Token is invalid or undefined');
+        if (!token || typeof token === 'undefined') {
+            console.info('DEU MERDA AQUI NA PORRA DO TOKEN DO YOUTOBA HEIN PQP!');
+            return undefined;
+        }
+        this.token = token;
+        return null;
     }
-     
-     return token;
-}
 
     async getAllPlaylists(_req: Request, res: Response) {
         // const decode = jwt.verify(token, JWT_SECRET);
-        const bearerToken = await this._getCredentials();
         try {
             // const decoded = jwt.verify(bearerToken, JWT_SECRET);
             console.warn('=============================== #GET_ALL_PLAYLISTS ==================================');
             // console.log("decode: ", decoded)
-            console.log('BEAR TOKEN: ', bearerToken);
-            console.log('MSG TOKEN: ', bearerToken);
+        
             const result = await axios.get(
                 `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails&mine=true&key=${API_KEY}`,
                 {
                     headers: {
-                        Authorization: `Bearer ${bearerToken}`,
+                        Authorization: `Bearer ${this.token}`,
                         Accept: 'application/json',
                         'Content-Type': 'application/json',
                     },
@@ -54,17 +53,17 @@ export class YoutubeService {
         }
     }
 
-    async removeDuplicatedItems(itemsToRemove: any) {
-        const success = itemsToRemove.map(async (itemId: any) => {
+    async removeDuplicatedItems(itemsToRemove: string[]) {
+        const requests = itemsToRemove.map(async (itemId: string) => {
             await axios.delete(`https://youtube.googleapis.com/youtube/v3/playlistItems?id=${itemId}&key=${API_KEY}`, {
                 headers: {
-                    Authorization: `Bearer ${this.bearerToken}`,
+                    Authorization: `Bearer ${this.token}`,
                     Accept: 'application/json',
                 },
             });
         });
 
-        const result = await Promise.all(success).catch((e: any) => console.error('Fudeo pra excluir os videos!\n', e));
+        const result = await Promise.all(requests).catch((e: any) => console.error('Fudeo pra excluir os videos!\n', e));
         return result;
     }
 }
