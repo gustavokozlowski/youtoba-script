786e8ae634baae429f4947282be8aa75d27dab64
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 01:30:09 -0300

    feat: add method get all playlists

--- src/services/youtube-playlist.services.ts
+++ src/services/youtube-playlist.services.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 import axios from 'axios';
+import { Response } from 'express';
+import { getToken } from '../utils/repository/user.repository';
 
 const { API_KEY } = process.env;
 // import { getToken } from "../utils/repository/user.repository";
@@ -17,6 +19,44 @@ export class YoutubeService {
     // }
 
     //  }
+
+    async getAllPlaylists (_req: Request, res: Response) {
+    // const decode = jwt.verify(token, JWT_SECRET);
+    const bearerToken = await getToken('bearerToken');
+
+    if (!bearerToken) {
+        return res.send('Token inválido! Por favor, faça o login novamente!');
+    }
+
+    try {
+        // const decoded = jwt.verify(bearerToken, JWT_SECRET);
+        console.warn('=============================== #GET_ALL_PLAYLISTS ==================================');
+        // console.log("decode: ", decoded)
+        console.log('BEAR TOKEN: ', bearerToken.token);
+        console.log('MSG TOKEN: ', bearerToken.message);
+        const result = await axios.get(
+            `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails&mine=true&key=${API_KEY}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${bearerToken.token}`,
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+
+        // console.info(result.headers)
+
+        return res.status(200).json({
+            mensagem: 'DEU BOM CARAAALHO!',
+            resultado: result.data,
+        });
+    } catch (e: any) {
+        console.error('Error daqueles', e.response);
+
+        return res.status(400).json({ messagem: 'deu merda no get heein' });
+    }
+};
 
     async removeDuplicatedItems(itemsToRemove: any) {
         const success = itemsToRemove.map(async (itemId: any) => {
