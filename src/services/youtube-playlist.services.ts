1e0ee3cbe4051ff332e6696976002d8df47ab153
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 01:43:37 -0300

    feat: add method getCredentials

--- src/services/youtube-playlist.services.ts
+++ src/services/youtube-playlist.services.ts
@@ -1,68 +1,64 @@
 require('dotenv').config();
-
 import axios from 'axios';
 import { Response } from 'express';
 import { getToken } from '../utils/repository/user.repository';
 
 const { API_KEY } = process.env;
-// import { getToken } from "../utils/repository/user.repository";
 
 export class YoutubeService {
-    constructor(public token: string) {}
+    bearerToken: string | undefined;
+    constructor() {
 
-    //  private async getCredentials(){
-    // const bearerToken =  getToken("bearerToken");
+        this.bearerToken = undefined;
+}
 
-    // if (!bearerToken.token || typeof bearerToken.token === undefined) {
-    //   console.info("DEU MERDA AQUI NA PORRA DO TOKEN DO YOUTOBA HEIN PQP!");
-    //   throw new Error('Token is invalid or undefined');
-    // }
+    private async _getCredentials(){
+    const { token } = await getToken("bearerToken");
 
-    //  }
-
-    async getAllPlaylists (_req: Request, res: Response) {
-    // const decode = jwt.verify(token, JWT_SECRET);
-    const bearerToken = await getToken('bearerToken');
-
-    if (!bearerToken) {
-        return res.send('Token inválido! Por favor, faça o login novamente!');
+    if (!token || typeof token === undefined) {
+      console.info("DEU MERDA AQUI NA PORRA DO TOKEN DO YOUTOBA HEIN PQP!");
+      throw new Error('Token is invalid or undefined');
     }
+     
+     return token;
+}
 
-    try {
-        // const decoded = jwt.verify(bearerToken, JWT_SECRET);
-        console.warn('=============================== #GET_ALL_PLAYLISTS ==================================');
-        // console.log("decode: ", decoded)
-        console.log('BEAR TOKEN: ', bearerToken.token);
-        console.log('MSG TOKEN: ', bearerToken.message);
-        const result = await axios.get(
-            `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails&mine=true&key=${API_KEY}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${bearerToken.token}`,
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
+    async getAllPlaylists(_req: Request, res: Response) {
+        // const decode = jwt.verify(token, JWT_SECRET);
+        const bearerToken = await this._getCredentials();
+        try {
+            // const decoded = jwt.verify(bearerToken, JWT_SECRET);
+            console.warn('=============================== #GET_ALL_PLAYLISTS ==================================');
+            // console.log("decode: ", decoded)
+            console.log('BEAR TOKEN: ', bearerToken);
+            console.log('MSG TOKEN: ', bearerToken);
+            const result = await axios.get(
+                `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails&mine=true&key=${API_KEY}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${bearerToken}`,
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
                 },
-            },
-        );
+            );
 
-        // console.info(result.headers)
+            return res.status(200).json({
+                mensagem: 'DEU BOM CARAAALHO!',
+                resultado: result.data,
+            });
+        } catch (e: any) {
+            console.error('Error daqueles', e.response);
 
-        return res.status(200).json({
-            mensagem: 'DEU BOM CARAAALHO!',
-            resultado: result.data,
-        });
-    } catch (e: any) {
-        console.error('Error daqueles', e.response);
-
-        return res.status(400).json({ messagem: 'deu merda no get heein' });
+            return res.status(400).json({ messagem: 'deu merda no get heein' });
+        }
     }
-};
 
     async removeDuplicatedItems(itemsToRemove: any) {
         const success = itemsToRemove.map(async (itemId: any) => {
             await axios.delete(`https://youtube.googleapis.com/youtube/v3/playlistItems?id=${itemId}&key=${API_KEY}`, {
                 headers: {
-                    Authorization: `Bearer ${this.token}`,
+                    Authorization: `Bearer ${this.bearerToken}`,
                     Accept: 'application/json',
                 },
             });
