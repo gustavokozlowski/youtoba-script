91981f1c2d8ce39a42b7f74c7814d9fc78029dde
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 03:07:20 -0300

    fix: format playlist router

--- src/routes/playlist.routes.ts
+++ src/routes/playlist.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { getAllPlaylists, getPlaylistItems } from '../controllers/playlist.controller';
 
-export const playlistRouter = Router();
+const playlistRouter = Router();
 
 playlistRouter.route('/get-all').get(getAllPlaylists);
-
 playlistRouter.route('/:playlistId').get(getPlaylistItems);
+
+export { playlistRouter };
