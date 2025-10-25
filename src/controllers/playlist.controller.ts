525e7cc39b9de5f4847e48d45c540fcd0af243ed
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 02:47:04 -0300

    fix: biome check

--- src/controllers/playlist.controller.ts
+++ src/controllers/playlist.controller.ts
@@ -154,7 +154,6 @@ export const getPlaylistItems = async (req: Request, res: Response) => {
     }
 };
 
-
 // REFATORAR EM BREVE!
 export const removeItemsDuplicated = (list: any[]) => {
     const newList = [] as any[];
