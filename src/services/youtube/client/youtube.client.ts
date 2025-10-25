72766d23ce0c351852b282ae7e376388316757a9
gustavokozlowski <guskozlowskidev@gmail.com>
2025-10-25 03:41:48 -0300

    feat: create youtube client for manager requeet

--- src/services/youtube/client/youtube.client.ts
+++ src/services/youtube/client/youtube.client.ts
@@ -1,13 +1,37 @@
-
-
-
+require('dotenv').config();
+import axios from 'axios';
+const { YOUTUBE_BASE_UR } = process.env;
 
 export class YoutubeClient {
     // Youtube client methods and properties here
+    private client;
     private apiKey: string;
+    private baseURL: string;
 
     constructor(apiKey: string) {
         this.apiKey = apiKey;
+        this.baseURL = YOUTUBE_BASE_UR ?? '';
+        this.client = axios.create({
+            baseURL: this.baseURL,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
     }
 
+
+    public async getPlaylists(token: string) {
+        try {
+            const response = await this.client.get(`/playlists?part=contentDetails&mine=true&key=${this.apiKey}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching playlists:', error);
+            throw error;
+        }
+    }
 }
