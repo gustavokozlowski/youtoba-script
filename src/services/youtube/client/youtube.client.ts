c07afc0fc1406c3a6ef1dd5f2839c9a6223b96be
gustavokozlowski <guskozlowskidev@gmail.com>
2025-11-25 22:01:06 -0300

    feat: create method playlistDetails for youtube client class

--- src/services/youtube/client/youtube.client.ts
+++ src/services/youtube/client/youtube.client.ts
@@ -1,17 +1,19 @@
 require('dotenv').config();
 
 import axios from 'axios';
-import type { GetPlaylistsResponse, YoutubeClientConfig } from './client.types';
+import type { PlaylistsResponse, YoutubeClientConfig, PlaylistDetailsResponse } from './client.types';
 
-const { YOUTUBE_BASE_URL } = process.env;
+const { YOUTUBE_BASE_URL, API_KEY } = process.env;
 
 export class YoutubeClient {
     // Youtube client methods and properties here
     private client;
     private apiKey: string;
+    private token: string;
 
     constructor({ apiKey, token }: YoutubeClientConfig) {
         this.apiKey = apiKey;
+        this.token = token;
         this.client = axios.create({
             baseURL: YOUTUBE_BASE_URL,
             headers: {
@@ -25,10 +27,25 @@ export class YoutubeClient {
     async playlists() {
         try {
             const response = await this.client.get(`playlists?part=contentDetails&mine=true&key=${this.apiKey}`);
-            return response.data as GetPlaylistsResponse;
+            return response.data as PlaylistsResponse;
         } catch (error: any) {
             console.error('Error fetching playlists:', error);
-            throw error;
+            return null;
         }
     }
+
+    async playlistDetails(playlistId: string) {
+        try {
+               const playlistInfo = await this.client.get(
+                `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`);
+
+            return playlistInfo.data as PlaylistDetailsResponse;
+            
+        } catch (error: any) {
+            console.error(`Deu merda para obter os detalhes da playlist: ${playlistId}`, error);
+            return null;
+        }
+     }
+
+
 }
