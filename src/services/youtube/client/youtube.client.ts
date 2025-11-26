54fb4d1c9610bc3991aabeced5deae6e78047d09
gustavokozlowski <guskozlowskidev@gmail.com>
2025-11-26 01:37:06 -0300

    feat: create method playlist for youtube client class

--- src/services/youtube/client/youtube.client.ts
+++ src/services/youtube/client/youtube.client.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 import axios from 'axios';
-import type { PlaylistDetailsResponse, PlaylistsResponse, YoutubeClientConfig } from './client.types';
+import type { PlaylistDetailsResponse, PlaylistResponse, PlaylistsResponse, YoutubeClientConfig } from './client.types';
 
 const { YOUTUBE_BASE_URL, API_KEY } = process.env;
 
@@ -21,23 +21,36 @@ export class YoutubeClient {
         });
     }
 
-    async playlists() {
+    async playlists(): Promise<PlaylistsResponse | null> {
         try {
             const response = await this.client.get(`playlists?part=contentDetails&mine=true&key=${this.apiKey}`);
             return response.data as PlaylistsResponse;
         } catch (error: any) {
-            console.error('Error fetching playlists:', error);
+            console.error('Erro ao obter playlists:', error);
             return null;
         }
     }
 
-    async playlistDetails(playlistId: string) {
+    async playlistDetails(playlistId: string): Promise<PlaylistDetailsResponse | null> {
         try {
             const playlistInfo = await this.client.get(
                 `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`,
             );
 
             return playlistInfo.data as PlaylistDetailsResponse;
+        } catch (error: any) {
+            console.error(`Deu merda para obter os detalhes da playlist: ${playlistId}`, error);
+            return null;
+        }
+    }
+
+    async playlist(playlistId: string): Promise<PlaylistResponse | null> {
+        try {
+            const playlist = await this.client.get(
+                `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${playlistId}&key=${API_KEY}`,
+            );
+
+            return playlist.data as PlaylistResponse;
         } catch (error: any) {
             console.error(`Deu merda para obter os detalhes da playlist: ${playlistId}`, error);
             return null;
