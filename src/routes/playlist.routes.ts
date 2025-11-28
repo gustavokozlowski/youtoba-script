7a80d049c36946d7534908821e47623fdc40e4df
gustavokozlowski <guskozlowskidev@gmail.com>
2025-11-28 02:03:43 -0300

    feat: add /remove-duplicates endpoint

--- src/routes/playlist.routes.ts
+++ src/routes/playlist.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllPlaylists, getPlaylistItems } from '../controllers/playlist.controller';
+import { getAllPlaylists, getPlaylistInfoById, removeDuplicatedItems } from '../controllers/playlist.controller';
 
 const playlistRouter = Router();
 
 playlistRouter.route('/get-all').get(getAllPlaylists);
-playlistRouter.route('/:playlistId').get(getPlaylistItems);
+playlistRouter.route('/:playlistId').get(getPlaylistInfoById);
+playlistRouter.route('/:playlistId/remove-duplicates').delete(removeDuplicatedItems);
 
 export { playlistRouter };
