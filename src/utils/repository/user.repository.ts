2e09810adad0689f0c411104113f3c4d177dfe56
gustavokozlowski <guskozlowskidev@gmail.com>
2026-01-08 01:20:13 -0300

    refactor: user repository

--- src/utils/repository/user.repository.ts
+++ src/utils/repository/user.repository.ts
@@ -1,23 +1,47 @@
-import NodeCache from 'node-cache';
+import fs from 'node:fs';
 import type { AccessToken } from './types';
 
-const db = new NodeCache();
-
 export function saveToken(name: string, value: string) {
-    const success = db.set(name, value);
-    if (success !== true) {
-        return success;
+    // Verifica se o arquivo existe
+    if (fs.existsSync('./config.json')) {
+        // Arquivo existe, sobrescreve com o novo valor
+        fs.writeFileSync(
+            './config.json',
+            JSON.stringify(
+                {
+                    [name]: value,
+                },
+                null,
+                2,
+            ),
+        );
+        return true;
     }
-    return success;
+
+    // Se não existe, cria um novo
+    fs.writeFileSync(
+        './config.json',
+        JSON.stringify(
+            {
+                [name]: value,
+            },
+            null,
+            2,
+        ),
+    );
+
+    return true;
 }
 
-export async function getToken(name: string): Promise<AccessToken> {
-    const token: string | undefined = await db.get(name);
+export  function getToken(name: string): AccessToken {
+    const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+    const token = config[name];
     if (token === undefined || !token) {
         return {
             message: 'Esse token não existe!',
         } as AccessToken;
     }
+
     return {
         message: 'Sucessada, #vadias! o token ta na mão!',
         token: token,
