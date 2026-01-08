faae2b9b775bb029f62d36fcd0bfc7f1f6ec0338
gustavokozlowski <guskozlowskidev@gmail.com>
2026-01-08 01:17:25 -0300

    feat: add auth middleware in  routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authRouter } from '../routes/auth.routes';
 import { playlistRouter } from '../routes/playlist.routes';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const routes = Router();
 
 routes.use('/playlist', playlistRouter);
 routes.use('/auth', authRouter);
+routes.use(authMiddleware);
 
 export { routes };
